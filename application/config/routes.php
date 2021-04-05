3f34f631292e161b81d97a19d89a6b256249d846
RaihanHeggi <syaraihanheggi@gmail.com>
2021-04-05 20:46:51 +0700

    Admin Template
    Implementation Admin Dashboard Template for Rakit Komputer Usage

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,5 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'IndexController';
 $route['login'] = 'LoginController';
 $route['halaman_index'] = 'IndexController';
+$route['index_admin'] = 'AdminController';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
