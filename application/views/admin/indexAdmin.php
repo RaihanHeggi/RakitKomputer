c87c14017ee5a7eb2b96869da9c76fb17374c881
RaihanHeggi <syaraihanheggi@gmail.com>
2021-04-20 20:00:50 +0700

    Admin Menu Barang
    Menyediakan UI Menu Cek Barang

--- application/views/admin/indexAdmin.php
+++ application/views/admin/indexAdmin.php
@@ -34,7 +34,7 @@
           </a>
         </li>
         <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Barang">
-          <a class="nav-link" href="#">
+          <a class="nav-link" href="<?= site_url('data_barang') ?>">
             <i class="fa fa-fw fa-area-chart"></i>
             <span class="nav-link-text">Barang</span>
           </a>
