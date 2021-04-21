8873c5d6718247d5c34e402c04149aaa4aafbfc8
arvaadwitya <47820177+arvaadwitya@users.noreply.github.com>
2021-04-21 08:58:44 +0700

    Update BarangController.php

--- application/controllers/BarangController.php
+++ application/controllers/BarangController.php
@@ -1,8 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class AdminController extends CI_Controller {
+class BarangController extends CI_Controller {
 
-
+    function __construct()
+    {
+		parent::__construct();
+		$this->load->model('barang_model','barang');
+        $this->load->helper('url');  
+    }
 
 }
