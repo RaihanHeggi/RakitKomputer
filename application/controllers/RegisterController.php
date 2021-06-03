0815f266131171d8c1a4bc261ad166780060194f
bramanp <71612936+bramanp@users.noreply.github.com>
2021-06-03 10:07:54 +0700

    Update RegisterController.php

--- application/controllers/RegisterController.php
+++ application/controllers/RegisterController.php
@@ -7,10 +7,10 @@ class RegisterController extends CI_Controller {
     {
 		parent::__construct();
         $this->load->model("user_model","user");
-		$this->load->model("pelanggan_model","pelanggan");
-		$this->load->model("admin_model","admin");
-		$this->load->model("konsultan_model","konsultan");
-		$this->load->model("manajer_model","manajer");
+	$this->load->model("pelanggan_model","pelanggan");
+	$this->load->model("admin_model","admin");
+	$this->load->model("konsultan_model","konsultan");
+	$this->load->model("manajer_model","manajer");
         #$this->load->model('modelRS');  
         #$this->load->model('LoginModel');
         $this->load->helper('url');  
@@ -20,7 +20,7 @@ class RegisterController extends CI_Controller {
 	{
 		$this->load->view('login/ViewRegister');
 	}
-
+	// test
 	public function registerData(){
 		$nama = $this->input->post('nama');
 		$email = $this->input->post('email'); 
