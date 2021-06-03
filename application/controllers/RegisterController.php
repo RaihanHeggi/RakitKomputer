1921171d854cef9de47e0fce1bce5307c66bec94
bramanp <71612936+bramanp@users.noreply.github.com>
2021-06-03 21:56:54 +0700

    Update RegisterController.php
    komentar dan penambahan function

--- application/controllers/RegisterController.php
+++ application/controllers/RegisterController.php
@@ -20,7 +20,53 @@ class RegisterController extends CI_Controller {
 	{
 		$this->load->view('login/ViewRegister');
 	}
-	// test
+	
+	# =================== fungsi - fungsi input data ===================
+	
+	function inputDataPelanggan($nama, $alamat, $email, $userID) {
+		$dataPelanggan = array(
+			'nama_pelanggan' => $nama,
+			'alamat_pelanggan' => $alamat,
+			'email_pelanggan' => $email,
+			'id_user' => $userID
+		);
+		$this->pelanggan->insertData($dataPelanggan);
+		$this->session->set_flashdata('error_messages',' <div><label for="Alert" style="color:green">Silahkan Melakukan Login</label></div>');
+	}
+	
+	function inputDataKonsultan($nama, $alamat, $email, $userID) {
+		$dataKonsultan = array(
+			'nama_konsultan' => $nama,
+			'alamat_konsultan' => $alamat,
+			'email_konsultan' => $email,
+			'id_user' => $userID
+		);
+		$this->konsultan->insertData($dataKonsultan);
+		$this->session->set_flashdata('error_messages',' <div><label for="Alert" style="color:green">Silahkan Melakukan Login</label></div>');
+	}
+	
+	function inputDataManajer($nama, $alamat, $userID) {
+		$dataManajer = array(
+			'nama_manajer' => $nama,
+			'alamat_manajer' => $alamat,
+			'id_user' => $userID
+		);
+		$this->manajer->insertData($dataManajer);
+		$this->session->set_flashdata('info', '<div><label for="Alert" style="color:green">Data Berhasil Ditambahkan</label></div>');
+	}
+	
+	function inputDataAdmin($nama, $alamat, $userID) {
+		$dataAdmin = array(
+			'nama_admin' => $nama,
+			'alamat_admin' => $alamat,
+			'id_user' => $userID
+		);
+		$this->admin->insertData($dataAdmin);
+		$this->session->set_flashdata('info', '<div><label for="Alert" style="color:green">Data Berhasil Ditambahkan</label></div>');
+	}
+
+	# ==================================================
+	
 	public function registerData(){
 		$nama = $this->input->post('nama');
 		$email = $this->input->post('email'); 
@@ -30,10 +76,10 @@ class RegisterController extends CI_Controller {
 		$role = $this->input->post('role');
 		$alamat = $this->input->post('alamat');
 
-		if($nama != NULL && $email != NULL && $username != NULL && $password != NULL && $role != NULL && $alamat != NULL){
-			if(!$this->user->cekData($username,$email))
+		if ($nama != NULL && $email != NULL && $username != NULL && $password != NULL && $role != NULL && $alamat != NULL){
+			if (!$this->user->cekData($username,$email))
 			{
-				if($password == $rePassword){
+				if ($password == $rePassword){
 					$dataUser = array(
 						'username' => $username,
 						'password' => $password,
@@ -41,49 +87,26 @@ class RegisterController extends CI_Controller {
 					);
 					$this->user->insertData($dataUser);
 					$userID = $this->user->getID($username,$email);
+					
+					#Input data sesuai role yang dipilih
 					if ($role == "Konsultan"){
-						$dataKonsultan = array(
-							'nama_konsultan' => $nama,
-							'alamat_konsultan' => $alamat,
-							'email_konsultan' => $email,
-							'id_user' => $userID
-						);
-						$this->konsultan->insertData($dataKonsultan);
-						$this->session->set_flashdata('error_messages',' <div><label for="Alert" style="color:green">Silahkan Melakukan Login</label></div>');
+						$this->inputDataKonsultan($nama, $alamat, $email, $userID);
 					}else if ($role == "Pelanggan"){
-						$dataPelanggan = array(
-							'nama_pelanggan' => $nama,
-							'alamat_pelanggan' => $alamat,
-							'email_pelanggan' => $email,
-							'id_user' => $userID
-						);
-						$this->pelanggan->insertData($dataPelanggan);
-						$this->session->set_flashdata('error_messages',' <div><label for="Alert" style="color:green">Silahkan Melakukan Login</label></div>');
+						$this->inputDataPelanggan($nama, $alamat, $email, $userID);
 					}else if ($role == "Manajer"){
-						$dataManajer = array(
-							'nama_manajer' => $nama,
-							'alamat_manajer' => $alamat,
-							'id_user' => $userID
-						);
-						$this->manajer->insertData($dataManajer);
-						$this->session->set_flashdata('info', '<div><label for="Alert" style="color:green">Data Berhasil Ditambahkan</label></div>');
+						$this->inputDataManajer($nama, $alamat, $userID);
 						redirect('tambah_data_admin');
 					}else if($role == "Admin"){
-						$dataAdmin = array(
-							'nama_admin' => $nama,
-							'alamat_admin' => $alamat,
-							'id_user' => $userID
-						);
-						$this->admin->insertData($dataAdmin);
-						$this->session->set_flashdata('info', '<div><label for="Alert" style="color:green">Data Berhasil Ditambahkan</label></div>');
+						$this->inputDataAdmin($nama, $alamat, $userID);
 						redirect('tambah_data_admin');
 					}
 					redirect('halaman_register');
-				}else{
+				#Jika password dan re-password tidak sama
+				} else { 
 					$this->session->set_flashdata('error_messages',' <div><label for="Alert" style="color:red">Password Tidak Sama</label></div>');
 					redirect('halaman_register');
 				}
-			}else{
+			} else {
 				if(($role == "Manajer" )||($role == "Admin")){
 					$this->session->set_flashdata('info', '<div><label for="Alert" style="color:red">Data User Sudah Ada</label></div>');
 					redirect('tambah_data_admin');
@@ -91,7 +114,8 @@ class RegisterController extends CI_Controller {
 				$this->session->set_flashdata('error_messages',' <div><label for="Alert" style="color:red">User Sudah Pernah Mendaftar</label></div>');
 				redirect('halaman_register');
 			}
-		}else{
+		#Jika terdapat input yang kosong
+		} else {
 			$this->session->set_flashdata('error_messages',' <div><label for="Alert" style="color:red">Input Harus Diisi</label></div>');
 			redirect('halaman_register');
 		}
