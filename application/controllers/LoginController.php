3f5734661722e5e6772ae4ec26c86686af2a9366
RaihanHeggi <syaraihanheggi@gmail.com>
2021-06-09 08:02:54 +0700

    Unit Testing
    Menambahkan Unit Testing Pada Login

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -11,6 +11,7 @@ class LoginController extends CI_Controller {
 		$this->load->model("admin_model","admin");
 		$this->load->model("konsultan_model","konsultan");
 		$this->load->model("manajer_model","manajer");
+		$this->load->library('unit_test');
         $this->load->helper('url');  
     }
 
@@ -97,4 +98,63 @@ class LoginController extends CI_Controller {
             redirect('login');
 		}
 	}
+
+	public function logout() {
+		$sess = $this->session->sess_destroy();
+		redirect('login', 'refresh');
+	}
+
+
+
+
+	// UNIT TESTING SECTION  
+	public function testLoginProses(){
+		//True Value 
+		$test_1 = $this->loginProses("heggi123",'heggi.sya@gmail.com','123',1);
+		$expected_1 = "Konsultan";
+		$test_name_1 = "Mengecek Fungsionalitas Login Proses Mengembalikan Status Konsultan";
+		$test_2 = $this->loginProses("hafidz123",'hafidz123@gmail.com','123',2);
+		$expected_2 = "Pelanggan";
+		$test_name_2 = "Mengecek Fungsionalitas Login Proses Mengembalikan Status Pelanggan";
+		$test_3 = $this->loginProses("hindia123",'hindia123@gmail.com','123',4);
+		$expected_3 = "Admin";
+		$test_name_3 = "Mengecek Fungsionalitas Login Proses Mengembalikan Status Admin";
+		$test_4 = $this->loginProses("arga123",'argaz123@gmail.com','123',5);
+		$expected_4 = "Manajer";
+		$test_name_4 = "Mengecek Fungsionalitas Login Proses Mengembalikan Status Manajer";
+		echo $this->unit->run($test_1,$expected_1,$test_name_1);
+		echo $this->unit->run($test_2,$expected_2,$test_name_2);
+		echo $this->unit->run($test_3,$expected_3,$test_name_3);
+		echo $this->unit->run($test_4,$expected_4,$test_name_4);
+
+		//False Value 
+		$test_5 = $this->loginProses("hafidz123",'hafidz123@gmail.com','123',2);
+		$expected_5 = "Konsultan";
+		$test_name_5 = "Mengecek Fungsionalitas Login Proses Mengembalikan Status Konsultan";
+		$test_6 = $this->loginProses("heggi123",'heggi.sya@gmail.com','123',1);
+		$expected_6 = "Pelanggan";
+		$test_name_6 = "Mengecek Fungsionalitas Login Proses Mengembalikan Status Pelanggan";
+		$test_7 = $this->loginProses("arga123",'argaz123@gmail.com','123',5);
+		$expected_7 = "Admin";
+		$test_name_7= "Mengecek Fungsionalitas Login Proses Mengembalikan Status Admin";
+		$test_8 = $this->loginProses("hindia123",'hindia123@gmail.com','123',4);
+		$expected_8 = "Manajer";
+		$test_name_8 = "Mengecek Fungsionalitas Login Proses Mengembalikan Status Manajer";
+		echo $this->unit->run($test_5,$expected_5,$test_name_5);
+		echo $this->unit->run($test_6,$expected_6,$test_name_6);
+		echo $this->unit->run($test_7,$expected_7,$test_name_7);
+		echo $this->unit->run($test_8,$expected_8,$test_name_8);
+	}
+
+	public function testRoleLogin(){
+		$test_1 = $this->setStatus("heggi123",'heggi.sya@gmail.com','123',"Sya Raihan Heggi","Konsultan");
+		$expected_1 = "Konsultan";
+		$test_name_1 = "Mengecek Fungsionalitas Set Role Mengembalikan Status Konsultan";
+		$test_2 = $this->setStatus("hafidz123",'hafidz123@gmail.com','123',"Hafidz","Pelanggan");
+		$expected_2 = "Pelanggan";
+		$test_name_2 = "Mengecek Fungsionalitas Set Role Mengembalikan Status Konsultan";
+		echo $this->unit->run($test_1,$expected_1,$test_name_1);
+		echo $this->unit->run($test_2,$expected_2,$test_name_2);
+	}
+
 }
